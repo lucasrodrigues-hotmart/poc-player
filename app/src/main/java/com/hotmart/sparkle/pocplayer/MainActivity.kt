09f58f2e02f026b35b958d31c8c63fb297a8d5a2
Lucas Uchoa Rodrigues <choa.lucas@gmail.com>
2020-03-09 17:45:57 -0300

    create playMedia

--- app/src/main/java/com/hotmart/sparkle/pocplayer/MainActivity.kt
+++ app/src/main/java/com/hotmart/sparkle/pocplayer/MainActivity.kt
@@ -1,12 +1,42 @@
 package com.hotmart.sparkle.pocplayer
 
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.source.hls.HlsMediaSource
+import com.google.android.exoplayer2.upstream.DataSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Util
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    private fun playMedia(mediaUrl : String) {
+        val dataSourceFactory: DataSource.Factory = DefaultDataSourceFactory(
+            this,
+            Util.getUserAgent(this, "yourApplicationName")
+        )
+
+        // This is the MediaSource representing the media to be played.
+        // This is the MediaSource representing the media to be played.
+        val videoSource: MediaSource = HlsMediaSource.Factory(dataSourceFactory)
+            .createMediaSource(Uri.Builder().path(mediaUrl).build())
+
+        // Prepare the player with the source.
+        val simpleExoPlayer = ExoPlayer.Builder(this).build().apply {
+            playWhenReady = true
+            prepare(videoSource)
+        }
+
+        playerView.player = simpleExoPlayer
     }
 }
