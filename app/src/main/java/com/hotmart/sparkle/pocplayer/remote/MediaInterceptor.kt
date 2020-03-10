7264196d01fe08b7524d91cd956d4c75bd3f0939
Ricardo Mello <ricardo.mello@hotmart.com>
2020-03-10 14:04:33 -0300

    media interceptor fix

--- app/src/main/java/com/hotmart/sparkle/pocplayer/remote/MediaInterceptor.kt
+++ app/src/main/java/com/hotmart/sparkle/pocplayer/remote/MediaInterceptor.kt
@@ -7,15 +7,13 @@ import okhttp3.Response
 
 class MediaInterceptor(private val context: Context) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
-        val response = chain.proceed(chain.request())
-        return chain.proceed(addHlsHeader(response))
+        return chain.proceed(addHlsHeader(chain.request()))
     }
 
-    private fun addHlsHeader(response: Response): Request {
+    private fun addHlsHeader(request: Request): Request {
         val prefs = context.getSharedPreferences("preferences_media", Context.MODE_PRIVATE)
 
-        return Request.Builder()
-            .url(response.request.url)
+        return request.newBuilder()
             .addHeader(REFERER, "https://api.sparkleapp.com.br/")
             .addHeader(COOKIE, prefs.getString(COOKIE, EMPTY) ?: EMPTY)
             .build()
