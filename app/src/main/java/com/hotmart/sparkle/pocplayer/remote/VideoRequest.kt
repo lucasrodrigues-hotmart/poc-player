dd65856f15f5a488197c5df78fdbdaf422729346
Lucas Uchoa Rodrigues <choa.lucas@gmail.com>
2020-03-17 16:26:10 -0300

    add retrofit

--- app/src/main/java/com/hotmart/sparkle/pocplayer/remote/VideoRequest.kt
+++ app/src/main/java/com/hotmart/sparkle/pocplayer/remote/VideoRequest.kt
@@ -2,14 +2,37 @@ package com.hotmart.sparkle.pocplayer.remote
 
 import android.content.Context
 import android.util.Log
+import com.google.gson.Gson
+import com.google.gson.annotations.SerializedName
 import com.hotmart.sparkle.pocplayer.Timer
 import okhttp3.*
 import org.json.JSONObject
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Path
+import retrofit2.http.Query
 import java.io.IOException
 import kotlin.time.ExperimentalTime
 
+
 @ExperimentalTime
 object VideoRequest {
+
+
+    fun retrofit( url: String, token: String){
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.sparkleapp.com.br/rest/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val service = retrofit.create(Api::class.java)
+
+        val result = service.getDownload("Bearer $token","207274" ).execute()
+    }
+
+
     fun perform(context: Context, url: String, token: String, listener: (String) -> Unit) {
         val request = Request.Builder().url("$url?redirect=false")
             .addHeader("Authorization", "Bearer $token")
@@ -52,3 +75,14 @@ object VideoRequest {
         })
     }
 }
+
+
+interface Api {
+    @GET("v2/news/{id}}/download")
+    fun getDownload(@Header("Authorization") token : String, @Path("id") id: String, @Query("redirect") redirect: Boolean ?= false): retrofit2.Call<ResponseVideo>
+}
+
+data class ResponseVideo(
+    @SerializedName("data")
+    val data : String
+)
